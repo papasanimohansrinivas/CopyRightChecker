7b38015d028a79ebffafef242f87fab23427a9d3
papasani\mohan/srinivas <papasani.mohansrinivas@gmail.com>
2018-01-11 03:39:22 -0800

    Add files via upload

--- project-version-2/setup.py
+++ project-version-2/setup.py
@@ -1,10 +1,27 @@
-from distutils.core import setup
-
-
-setup(
-    name='CopyRightChecker',
-    version='0.1dev',
-    packages=['CopyRightChecker',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-)
+from setuptools import setup, find_packages
+ 
+ 
+ 
+setup(name='CopyRightChecker',
+ 
+      version='0.0.1',
+ 
+      # url='https://github.com/papasanimohansrinivas/IBM-CopyRightChecker',
+      url='https://pypi.python.org/pypi/CopyRightChecker/',
+ 
+      license='MIT',
+ 
+      author='Papasani Mohan',
+ 
+      author_email='papasani.mohansrinivas@gmail.com',
+ 
+      description='AppendCopyRighttoSourceFiles',
+ 
+      packages=find_packages(),
+ 
+      long_description=open('README.txt').read(),
+ 
+      zip_safe=True,
+ 
+      setup_requires=['gitPython'],
+      )
